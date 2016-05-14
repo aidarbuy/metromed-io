55a0d1217e09b6207cb1f886013a1dbbad509c4f
aidarbuy <aidarbuy@gmail.com>
2016-05-14 08:49:04 +0600

    Copy from https_server.js to server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var options = {
 	key:  fs.readFileSync('./key.pem'),
 	cert: fs.readFileSync('./cert.pem')
 };
-var https = require('https');
+var https = require('http');
 var HOST = 'localhost';
 var PORT = process.env.PORT || 4200;
-var server = https.createServer(options, app).listen(PORT);
-// var server = https.createServer(app).listen(PORT);
+// var server = https.createServer(options, app).listen(PORT);
+var server = https.createServer(app).listen(PORT);
 console.log('HTTPS Server listening on %s:%s', HOST, PORT);
 
 // routes
@@ -27,12 +27,12 @@ io.sockets.on('connection', function(socket) { // При подключении
 	socket.on('client event', function(data) {
 		console.log('timer: '+Date.now()+', ', data);
 	});
-	// При получении сообщения 'offer',
+	// При получении сообщения 'offer':
 	socket.on('offer', function(data) { // т.к. клиентское соединение одно,
 		// отправим сообщение обратно через тот же сокет
-		socket.emit('offer', data);
+		// socket.emit('offer', data);
 		// Если необходимо переслать сообщение по всем соединениям, кроме отправителя:
-		// socket.broadcast.emit('offer', data);
+		socket.broadcast.emit('offer', data);
 	});
 	socket.on('answer', function(data) {
 		socket.emit('answer', data);
